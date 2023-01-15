c008d7a77ece389ddbf3db27d634e04b91c97b19
JHuaWei <2237268069@qq.com>
2023-01-16 02:02:56 +0800

    完成 网站地图

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -21,6 +21,36 @@
 
                 <div class="map">网站地图
                     <div class="mapIcon"></div>
+                    <div class="mapList">
+                        <ul class="main">
+                            <li class="mainTitle">主站</li>
+                            <li class="first"><a>电竞虎首页</a></li>
+                            <li><a>游戏资讯</a></li>
+                            <li><a>直播导航</a></li>
+                            <li><a>游戏视频</a></li>
+                            <li><a>热门主播</a></li>
+                            <li><a>表情包图库</a></li>
+                            <li><a>APP下载</a></li>
+                        </ul>
+                        <ul class="exportsGames">
+                            <li class="exportsGamesTitle">电竞游戏</li>
+                            <li class="first"><a href="">英雄联盟</a></li>
+                            <li><a href="">守望先锋</a></li>
+                            <li><a href="">绝地求生</a></li>
+                            <li><a href="">炉石传说</a></li>
+                            <li><a href="">魔兽世界</a></li>
+                            <li><a href="">穿越火线</a></li>
+                            <li><a href="">地下城与勇士</a></li>
+                        </ul>
+                        <ul class="mobileGames">
+                            <li class="mobileGamesTitle">手机游戏</li>
+                            <li class="first"> <a href="">球球大作战</a></li>
+                            <li><a href="">皇室战争</a></li>
+                            <li><a href="">王者荣耀</a></li>
+                            <li><a href="">阴阳师</a></li>
+                        </ul>
+                    </div>
+
                 </div>
                 <input class="search" placeholder="搜索你想要的" />
                 <div class="searchIcon">
